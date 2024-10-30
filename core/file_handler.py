c4950ab13865c33c65cec3dba4d88cc8c26cd993
yxc0915 <62410385+yxc0915@users.noreply.github.com>
2024-10-30 14:37:16 +0800

    保存上传图片为PNG格式

--- core/file_handler.py
+++ core/file_handler.py
@@ -5,17 +5,25 @@ import io
 def save_uploaded_files(uploaded_files, upload_folder):
     saved_files = []
     for uploaded_file in uploaded_files:
-        # 创建一个唯一的文件名
-        file_name = os.path.join(upload_folder, uploaded_file.name)
+        # 获取文件名和扩展名
+        name, ext = os.path.splitext(uploaded_file.name)
+        
+        # 创建一个唯一的文件名，使用.png作为新的扩展名
+        file_name = os.path.join(upload_folder, f"{name}.png")
         counter = 1
         while os.path.exists(file_name):
-            name, ext = os.path.splitext(uploaded_file.name)
-            file_name = os.path.join(upload_folder, f"{name}_{counter}{ext}")
+            file_name = os.path.join(upload_folder, f"{name}_{counter}.png")
             counter += 1
 
-        # 保存文件
-        with open(file_name, "wb") as f:
-            f.write(uploaded_file.getbuffer())
+        # 打开上传的图片
+        image = Image.open(io.BytesIO(uploaded_file.getbuffer()))
+        
+        # 如果图片模式不是RGB，转换为RGB
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
+        
+        # 保存为PNG格式
+        image.save(file_name, 'PNG')
         
         saved_files.append(file_name)
     
